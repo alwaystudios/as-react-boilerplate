eac677a14725b630579f3ed587072e7d6827eff8
Gary Alway <garyalway@gmail.com>
2021-08-14 21:32:53 +0100

    minor

--- src/components/Sockets.tsx
+++ src/components/Sockets.tsx
@@ -1,10 +1,11 @@
 import React, { useState } from 'react'
 
+const ws = new WebSocket('ws://localhost:3001')
+
 export const Sockets: React.FC = () => {
   const [message, setMessage] = useState<string>('')
   const [serverMessage, setServerMessage] = useState<string>('')
 
-  const ws = new WebSocket('ws://localhost:3001')
   // eslint-disable-next-line functional/immutable-data
   ws.onmessage = (event) => setServerMessage(event.data)
 
